9098240886282d93c0e34d3f105e5f8a43584beb
jefe_dz <amirotalbi94@gmail.com>
2021-05-29 21:08:54 +0200

    about 100%

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
 import '../styles/About.css';
+import linkedin from '../assets/linkedin.png';
+import github from '../assets/github.png';
 
 function About() {
     return (
         <div id="#about">
-            Hey I am the about anchor ✌
+            <br/>
+            <div className="about-title">
+                <span className="about-title-first">AMIR</span> 
+                <span className="about-title-second">TALBI</span>
+                </div>
+                <br/>
+            <span className="about-job">Student in computer science</span>
+            <div className="logos">
+                <a href="https://www.linkedin.com/in/amir-talbi-598082206/"><img src={linkedin} alt="linkedin logo" className="linkedin" /></a> &nbsp;
+                <a href="https://github.com/jefedz"><img src={github} alt="github logo" className="github" /></a>
+            </div>
+            <p className="description">I am a young geek 🤓 absorbed in new technologies !</p>
+             
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     return (<div className="header">
         <img src={logo} alt="logo" className="header-logo" />
         <ul className="header-list">
-            <a href="./About#about"><li>ABOUT</li></a>
+            <a href="#about"><li>ABOUT</li></a>
             <a href="#education"><li>EDUCATION</li></a>
             <a href="#skills"><li>SKILLS</li></a>
             <a href="#contact"><li>CONTACT</li></a>
